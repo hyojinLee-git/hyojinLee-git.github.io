9774fc2f0f0b445284f9c194204789cd42f01cdc
hyojinLee-git <lily960310@naver.com>
2021-12-21 22:23:04 +0900

    modify: drop down position

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,53 +1,62 @@
 import React from 'react';
-import styled from '@emotion/styled';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 const DropdownContainer=styled.div`
     position: absolute;
     left: 15px;
     top: 5%;
-    
     background-color: white;
-    padding:5px 10px;
+    padding:5px 0;
     border-radius: 5px;
     border-top-left-radius: 0;
     border:1px solid;
     border-top:none;
     box-sizing: border-box;
     z-index: 999;
+    @media (min-height:640px){
+        top:32px;
+    }
+    
     
 `
 
 const Ul=styled.ul`
     list-style: none;
     padding:0;
-    margin:0
+    margin:0;
+    & > hr{
+        margin:8px 10px;
+    }
 `
 const Li=styled.li`
     cursor: pointer;
+    padding:0 10px;
     &:hover{
         background-color: skyblue;
     }
 
 `
-const menuItems=[
-    'About This Mac',
-    'System Preferences',
-    'App Store',
-    '--------------------',
-    'Sleep',
-    'Restart',
-    'Shut Down',
-    '--------------------',
-    'Lock Screen',
-    'Log Out HyoJin'
-]
+
 const Dropdown = () => {
+    const navigate=useNavigate();
+    const goEnter=()=>{
+        navigate('/enter')
+    }
+
     return (
         <DropdownContainer>
             <Ul>
-                {
-                    menuItems.map((menuItem,idx)=><Li key={idx}>{menuItem}</Li>)
-                }
+                <Li>About This Mac</Li>
+                <Li>System Preferences</Li>
+                <Li>App Store</Li>
+                <hr/>
+                <Li>Sleep</Li>
+                <Li>Restart</Li>
+                <Li>Shut Down</Li>
+                <hr/>
+                <Li>Lock Screen</Li>
+                <Li onClick={goEnter}>Log Out HyoJin</Li>
             </Ul>
         </DropdownContainer>
     );
--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import styled from '@emotion/styled'
+import React from 'react';
+import styled from 'styled-components'
 import {FaApple,FaSearch,FaWifi} from 'react-icons/fa'
 
 
